92124812894f21eb505755eebfa0f9a2cdc04a5a
Misaka <1198890085@qq.com>
2025-09-25 10:38:26 +0800

    feat: 支持图片点击预览、图片去重、格式过滤与删除选中图片

--- gui/main_window.py
+++ gui/main_window.py
@@ -25,20 +25,25 @@ class WatermarkMainWindow(QMainWindow):
 
         # 左侧：图片列表
         left_layout = QVBoxLayout()
-        self.list_widget = QListWidget()
-        self.list_widget.setIconSize(Qt.QSize(100, 100))
-        self.list_widget.setSelectionMode(QAbstractItemView.ExtendedSelection)
-        left_layout.addWidget(QLabel("已导入图片："))
-        left_layout.addWidget(self.list_widget)
 
-        btn_layout = QHBoxLayout()
-        btn_import = QPushButton("导入图片")
-        btn_import.clicked.connect(self.import_images)
-        btn_folder = QPushButton("导入文件夹")
-        btn_folder.clicked.connect(self.import_folder)
-        btn_layout.addWidget(btn_import)
-        btn_layout.addWidget(btn_folder)
-        left_layout.addLayout(btn_layout)
+    self.list_widget = QListWidget()
+    self.list_widget.setIconSize(Qt.QSize(100, 100))
+    self.list_widget.setSelectionMode(QAbstractItemView.ExtendedSelection)
+    self.list_widget.itemSelectionChanged.connect(self.on_image_selected)
+    left_layout.addWidget(QLabel("已导入图片："))
+    left_layout.addWidget(self.list_widget)
+
+    btn_layout = QHBoxLayout()
+    btn_import = QPushButton("导入图片")
+    btn_import.clicked.connect(self.import_images)
+    btn_folder = QPushButton("导入文件夹")
+    btn_folder.clicked.connect(self.import_folder)
+    btn_delete = QPushButton("删除选中")
+    btn_delete.clicked.connect(self.delete_selected)
+    btn_layout.addWidget(btn_import)
+    btn_layout.addWidget(btn_folder)
+    btn_layout.addWidget(btn_delete)
+    left_layout.addLayout(btn_layout)
 
         main_layout.addLayout(left_layout, 2)
 
@@ -65,10 +70,14 @@ class WatermarkMainWindow(QMainWindow):
             self.add_images(files)
 
     def add_images(self, files):
+        exts = ('.jpg', '.jpeg', '.png', '.bmp', '.tiff')
         for f in files:
+            if not f.lower().endswith(exts):
+                continue
             if f not in self.image_list and os.path.isfile(f):
                 self.image_list.append(f)
                 item = QListWidgetItem(os.path.basename(f))
+                item.setData(Qt.UserRole, f)
                 thumb = self.make_thumbnail(f)
                 if thumb:
                     item.setIcon(QIcon(thumb))
@@ -83,6 +92,35 @@ class WatermarkMainWindow(QMainWindow):
             return thumb_path
         except Exception:
             return None
+
+    def on_image_selected(self):
+        selected = self.list_widget.selectedItems()
+        if not selected:
+            self.preview_label.clear()
+            self.preview_label.setText("预览区")
+            return
+        # 只预览第一个选中项
+        item = selected[0]
+        img_path = item.data(Qt.UserRole)
+        if img_path and os.path.isfile(img_path):
+            pixmap = QPixmap(img_path)
+            if not pixmap.isNull():
+                scaled = pixmap.scaled(600, 600, Qt.KeepAspectRatio, Qt.SmoothTransformation)
+                self.preview_label.setPixmap(scaled)
+            else:
+                self.preview_label.setText("无法加载图片")
+        else:
+            self.preview_label.setText("预览区")
+
+    def delete_selected(self):
+        selected = self.list_widget.selectedItems()
+        for item in selected:
+            row = self.list_widget.row(item)
+            img_path = item.data(Qt.UserRole)
+            if img_path in self.image_list:
+                self.image_list.remove(img_path)
+            self.list_widget.takeItem(row)
+        self.on_image_selected()
 
     def dragEnterEvent(self, event):
         if event.mimeData().hasUrls():
